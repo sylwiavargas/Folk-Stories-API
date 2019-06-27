cd3482e68c2e137f249a59a0acfe4d1f13e1a892
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-06-27 17:53:37 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+   namespace :v1 do
+     resources :connections#, only: [:index, :update]
+     resources :continents#, only: [:index, :update]
+     resources :countries#, only: [:index, :update]
+     resources :eps#, only: [:index, :update]
+     resources :ets#, only: [:index, :update]
+     resources :events#, only: [:index, :update]
+     resources :people#, only: [:index, :update]
+     resources :subscriptions#, only: [:index, :update]
+     resources :places#, only: [:index, :update]
+     resources :types#, only: [:index, :update]
+     resources :users#, only: [:index, :update]
+   end
+ end
 end
