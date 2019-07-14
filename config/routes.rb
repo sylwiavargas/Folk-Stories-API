ecefe3a9af30d3df492983bcade686c550dabc50
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-07-14 16:26:46 -0400

    cleaning routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,23 @@
 Rails.application.routes.draw do
   namespace :api do
    namespace :v1 do
-     resources :connections#, only: [:index, :update]
-     resources :continents#, only: [:index, :update]
-     resources :countries#, only: [:index, :update]
-     resources :eps#, only: [:index, :update]
-     resources :ets#, only: [:index, :update]
-     resources :cps
-     resources :events#, only: [:index, :update]
-     resources :bios, controller: 'people' #, only: [:index, :update]
-     resources :subscriptions#, only: [:index, :update]
-     resources :places#, only: [:index, :update]
-     resources :types#, only: [:index, :update]
-     resources :users#, only: [:index, :update, :create]
-     resources :days
-     resources :months
-     resources :pps
-     resources :years
+     resources :connections, only: [:index, :shpw, :update, :create]
+     # resources :continents#, only: [:index, :update]
+     # resources :countries#, only: [:index, :update]
+     # resources :eps#, only: [:index, :update]
+     # resources :ets#, only: [:index, :update]
+     # resources :cps
+     resources :events, only: [:index, :shpw, :update, :create]
+     resources :bios, controller: 'people', only: [:index, :shpw, :update, :create]
+     # resources :subscriptions#, only: [:index, :update]
+     # resources :places#, only: [:index, :update]
+     # resources :types#, only: [:index, :update]
+     resources :users
+     resources :months, only: [:index, :shpw, :update, :create]
+     # resources :days
+     # resources :months
+     resources :pps, only: [:index, :shpw, :update, :create]
+     # resources :years
      resources :dates, controller: 'month_days', param: :mdd
      post '/login', to: 'auth#create'
      get '/profile', to: 'users#profile'
