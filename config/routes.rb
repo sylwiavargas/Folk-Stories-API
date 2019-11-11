bab470f8d38e4e29d6b86196ae1f35971377035d
sylwiavargas <sylwia.vargas@gmail.com>
2019-11-10 22:47:08 -0500

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,28 @@
 Rails.application.routes.draw do
   namespace :api do
-   namespace :v1 do
-     resources :connections, only: [:index, :show, :update, :create]
-     # resources :continents#, only: [:index, :update]
-     # resources :countries#, only: [:index, :update]
-     # resources :eps#, only: [:index, :update]
-     # resources :ets#, only: [:index, :update]
-     # resources :cps
-     resources :events
-     resources :bios, controller: 'people', only: [:index, :show, :update, :create]
-     # resources :subscriptions#, only: [:index, :update]
-     # resources :places#, only: [:index, :update]
-     # resources :types#, only: [:index, :update]
-     resources :users
-     resources :months, only: [:index, :show, :update, :create]
-     # resources :days
-     # resources :months
-     resources :pps, only: [:index, :show, :update, :create]
-     # resources :years
-     resources :dates, controller: 'month_days', param: :mdd
-     post '/login', to: 'auth#create'
-     get '/profile', to: 'users#profile'
-     get '/reauth', to: "auth#reauth"
-   end
- end
+  namespace :v1 do
+    resources :connections, only: [:index, :show, :update, :create]
+    resources :events
+    resources :bios, controller: 'people', only: [:index, :show, :update, :create]
+    resources :users
+    resources :months, only: [:index, :show, :update, :create]
+    resources :pps, only: [:index, :show, :update, :create]
+    resources :dates, controller: 'month_days', param: :mdd
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+      get '/reauth', to: "auth#reauth"
+    end
+  end
 end
+
+    # resources :continents#, only: [:index, :update]
+    # resources :countries#, only: [:index, :update]
+    # resources :eps#, only: [:index, :update]
+    # resources :ets#, only: [:index, :update]
+    # resources :cps
+    # resources :subscriptions#, only: [:index, :update]
+    # resources :places#, only: [:index, :update]
+    # resources :types#, only: [:index, :update]
+    # resources :days
+    # resources :months
+    # resources :years
